59e999c0cb5e455f73073ca5369abe92893f54ab
Daniel <daniel.ungurean2@gmail.com>
2023-10-31 09:07:47 +0200

    First commit

---
+++ src/test/java/md/ceiti/techshop/TechShopApplicationTests.java
@@ -0,0 +1,13 @@
+package md.ceiti.techshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
